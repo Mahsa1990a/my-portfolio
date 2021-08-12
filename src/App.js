2b6db533975ab6a1e8fb8b14d9932bbed8f72c13
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-08-12 11:28:08 -0400

    import our bootstrap through node-modules folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-
 import './App.css';
+// we import bootstrap base on node-modules path(because each package we install will go in node-modules)
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
 
 function App() {
   return (
