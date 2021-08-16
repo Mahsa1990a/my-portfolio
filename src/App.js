63c37084d6b3cef8fda181101f2e7bbd02c9c050
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-08-16 10:41:51 -0400

    added header and background pic and styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 // we import bootstrap base on node-modules path(because each package we install will go in node-modules)
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
 import Navbar from './component/Navbar';
+import Header from './component/Header';
 
 function App() {
   return (
-    
-    <Navbar />
+    <>
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
---
+++ src/component/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="head-wraper">
+      <div className="blur">
+        <div className="main-info">
+          <h1>Web Development and Websites Promotions</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
