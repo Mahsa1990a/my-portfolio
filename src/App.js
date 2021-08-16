69d16006cb7542b17d7d8bc1ff828503e4b36cf9
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-08-16 11:21:32 -0400

    import particles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 // we import bootstrap base on node-modules path(because each package we install will go in node-modules)
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
+import Particles from "react-particles-js";
+
 import Navbar from './component/Navbar';
 import Header from './component/Header';
 
 function App() {
   return (
     <>
+      <Particles />
       <Navbar />
       <Header />
     </>
