f63a790e785c56804d16abe9990802fe39fc81a7
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-08-17 19:14:08 -0400

    added more styles

--- src/component/AboutMe.jsx
+++ src/component/AboutMe.jsx
@@ -6,13 +6,16 @@ const AboutMe = () => {
     <div className="container py-5">
       <div className="row">
         <div className="col-lg-6 col-xm-12">
-          <img src={Picture} alt="Author" />
+          <div className="photo-wrap">
+            <img className="profile-img" src={Picture} alt="Author" />
+          </div>
         </div>
         <div className="col-lg-6 col-xm-12">
           <h1>About Me</h1>
-          <p>
-            I am a junior Full-Stack Web Developer with six years professional background as an Electronics Engineer.
-            I get to be a Mentor for Lighthouse Labs to teach students who are excited about 12-weeks intensive "Web Development Bootcamp" that covers a variety of frontend and backend technologies.
+          <br></br>
+          <p className="about-me-text">
+            I am a junior Full-Stack Web Developer with six years professional background as an Electronics Engineer.<br></br><br></br>
+            I get to be a <strong>Mentor</strong> for Lighthouse Labs to teach students who are excited about 12-weeks intensive "Web Development Bootcamp" that covers a variety of frontend and backend technologies.<br></br>
             Working as a Web Developer helps me to write well-designed, testable, efficient code by using best software development practices.
             I am always eager to learn about new technologies and practice for building robust, scalable software solutions in collaboration with team members.
           </p>
