f83d848ee965d6f8cc07c91b3057422f06642a29
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-08-18 09:25:20 -0400

    added more styles

--- src/component/Education.jsx
+++ src/component/Education.jsx
@@ -9,18 +9,18 @@ const Education = () => {
             <div className="col-md-6 col-sm-6">
               <div className="box">
                 <h3>Diploma of Full Stack Web Development</h3>
-                <h5>Lighthouse Labs (Feb 2021)</h5>
-                <p>Collaborative development, pair programming, team version control<br></br>
+                <h5>Lighthouse Labs (Feb 2021)</h5><br></br><br></br>
+                <h6>Collaborative development, pair programming, team version control<br></br>
                   Computer science fundamentals: OOP, abstraction, recursion, algorithms
-                </p>
+                </h6>
               </div>
             </div>
             {/* - */}
             <div className="col-md-6 col-sm-6">
               <div className="box">
                 <h3>Bachelor of Science in Electronics Engineering</h3>
-                <h5>IAU, Tehran, Iran (2010)</h5>
-                <p>(Canadian Equivalency, as assessed by Professional Engineers Ontario)</p>
+                <h5>IAU, Tehran, Iran (2010)</h5><br></br>
+                <h6>(Canadian Equivalency, as assessed by Professional Engineers Ontario)</h6>
               </div>
             </div>
           </div>
