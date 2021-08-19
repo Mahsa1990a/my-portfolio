249c9bf85913a281fc9d3e4f1a49b6e4c55e92d3
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-08-19 10:53:06 -0400

    copy and paste it for another projects

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -17,11 +17,11 @@ const Projects = () => {
     const content = (
     <>
       <img className="project-image-popupbox" src={Project1} alt="Reflexion" />
-      <p className="project-reflexion-desc">
+      <p className="project-desc">
         React based project to create a yoga studio at home<br></br>
-        Tech Stack: React, Axios, Node.js, Express, Node-postgres, Tensorflow.js
+        <strong>Tech Stack:</strong> React, Axios, Node.js, Express, Node-postgres, Tensorflow.js
       </p>
-      <b className="github">Github: </b><a className="hyper-link" onClick={() => window.open("https://github.com/Luke-Berzins/Reflexion")}>https://github.com/Luke-Berzins/Reflexion</a>
+      <b className="github">Github: </b><a className="hyper-link" onClick={() => window.open("https://github.com/Luke-Berzins/Reflexion", "_blank")}>https://github.com/Luke-Berzins/Reflexion</a>
     </>
     )
     PopupboxManager.open({ content })
@@ -31,6 +31,56 @@ const Projects = () => {
     titleBar: {
       enable: true,
       text: "Reflexion Project"
+    },
+    // Effect for popupbox:
+    fadeIn: true,
+    fadeInSpeed: 500
+  }
+
+  // Scheduler
+  const openPopupboxScheduler = () => {
+    const content = (
+    <>
+      <img className="project-image-popupbox" src={Project2} alt="Scheduler" />
+      <p className="project-desc">
+        React based project to create meetings for students to connect with mentors in real-time<br></br>
+        <strong>Tech Stack:</strong> React, Axios, Classnames, Normalize, Storybook
+      </p>
+      <b className="github">Github: </b><a className="hyper-link" onClick={() => window.open("https://github.com/Mahsa1990a/scheduler", "_blank")}>https://github.com/Mahsa1990a/scheduler</a>
+    </>
+    )
+    PopupboxManager.open({ content })
+  };
+
+  const popupboxConfigScheduler =  {
+    titleBar: {
+      enable: true,
+      text: "Scheduler Project"
+    },
+    // Effect for popupbox:
+    fadeIn: true,
+    fadeInSpeed: 500
+  }
+
+  // Tweeter
+  const openPopupboxTweeter = () => {
+    const content = (
+    <>
+      <img className="project-image-popupbox" src={Project3} alt="Tweeter" />
+      <p className="project-desc">
+      Single page Twitter clone<br></br>
+        <strong>Tech Stack:</strong> HTML, CSS, JS, jQuery and AJAX, Node, Express and MongoDB
+      </p>
+      <b className="github">Github: </b><a className="hyper-link" onClick={() => window.open("https://github.com/Mahsa1990a/tweeter", "_blank")}>https://github.com/Mahsa1990a/tweeter</a>
+    </>
+    )
+    PopupboxManager.open({ content })
+  };
+
+  const popupboxConfigTweeter =  {
+    titleBar: {
+      enable: true,
+      text: "Tweeter Project"
     },
     // Effect for popupbox:
     fadeIn: true,
@@ -48,13 +98,13 @@ const Projects = () => {
             <FontAwesomeIcon className="project-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
-          <div className="project-image-box">
+          <div className="project-image-box" onClick={openPopupboxScheduler}>
             <img className="project-image image2" src={Project2} alt="Project2" />
             <div className="overflow"></div>
             <FontAwesomeIcon className="project-icon" icon={faSearchPlus} />
           </div>
           {/* - */}
-          <div className="project-image-box">
+          <div className="project-image-box" onClick={openPopupboxTweeter}>
             <img className="project-image image3" src={Project3} alt="Project3" />
             <div className="overflow"></div>
             <FontAwesomeIcon className="project-icon" icon={faSearchPlus} />
@@ -62,6 +112,8 @@ const Projects = () => {
         </div>
       </div>
       <PopupboxContainer { ...popupboxConfigReflexion }/>
+      <PopupboxContainer { ...popupboxConfigScheduler }/>
+      <PopupboxContainer { ...popupboxConfigTweeter }/>
     </div>
   )
 }
