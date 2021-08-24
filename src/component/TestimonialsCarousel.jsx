9cf9e1414ff70f78093ca10f45a1fe6fed54afbc
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-08-24 09:20:00 -0400

    added avatar

--- src/component/TestimonialsCarousel.jsx
+++ src/component/TestimonialsCarousel.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+// Import the react-responsive-carousel package from node moduls
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+// Avatar imports
+import Avatar from "../Avatar.webp";
+ 
 const TestimonialsCarousel = () => {
   return (
-    <div>
-      
-    </div>
+    <Carousel>
+      <>
+        <img src={Avatar} alt="Avatar John Do1"/>
+        <h3>John Do1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo odit quas, voluptates iure eaque ab molestias fugiat velit! Officiis, fugit.</p>
+      </>
+      <>
+        <img src={Avatar} alt="Avatar John Do2"/>
+        <h3>John Do2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo odit quas, voluptates iure eaque ab molestias fugiat velit! Officiis, fugit.</p>
+      </>
+      <>
+        <img src={Avatar} alt="Avatar John Do3"/>
+        <h3>John Do3</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo odit quas, voluptates iure eaque ab molestias fugiat velit! Officiis, fugit.</p>
+      </>
+    </Carousel>
   )
 }
 
