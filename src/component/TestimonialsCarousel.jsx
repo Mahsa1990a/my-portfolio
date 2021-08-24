c0047f7ff726f99bdfdcb7e28332a5f442d2734d
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-08-24 10:54:21 -0400

    cover h3 and p with a div and style it

--- src/component/TestimonialsCarousel.jsx
+++ src/component/TestimonialsCarousel.jsx
@@ -11,18 +11,24 @@ const TestimonialsCarousel = () => {
     <Carousel>
       <>
         <img src={Avatar} alt="Avatar John Do1"/>
-        <h3>John Do1</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo odit quas, voluptates iure eaque ab molestias fugiat velit! Officiis, fugit.</p>
+        <div className="myCarousel">
+          <h3>John Do1</h3>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo odit quas, voluptates iure eaque ab molestias fugiat velit! Officiis, fugit.</p>
+        </div>
       </>
       <>
         <img src={Avatar} alt="Avatar John Do2"/>
-        <h3>John Do2</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo odit quas, voluptates iure eaque ab molestias fugiat velit! Officiis, fugit.</p>
+        <div className="myCarousel">
+          <h3>John Do2</h3>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo odit quas, voluptates iure eaque ab molestias fugiat velit! Officiis, fugit.</p>
+        </div>
       </>
       <>
         <img src={Avatar} alt="Avatar John Do3"/>
-        <h3>John Do3</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo odit quas, voluptates iure eaque ab molestias fugiat velit! Officiis, fugit.</p>
+        <div className="myCarousel">
+          <h3>John Do3</h3>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo odit quas, voluptates iure eaque ab molestias fugiat velit! Officiis, fugit.</p>
+        </div>
       </>
     </Carousel>
   )
